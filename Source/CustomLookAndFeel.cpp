f255643c19272ff860522cdde73248f7ce148bb7
Stéphane Bressani <stephane@bressani.dev>
2023-10-23 16:29:39 +0200

    avancée dans la mise en place des champs

--- Source/CustomLookAndFeel.cpp
+++ Source/CustomLookAndFeel.cpp
@@ -8,13 +8,18 @@ void CustomLookAndFeel::positionComboBoxText (juce::ComboBox& box, juce::Label& 
     // Ne faites rien ici. Cela empêchera JUCE de positionner et de dessiner le label par défaut.
 }
 
-void CustomLookAndFeel::drawComboBox(juce::Graphics& g, int width, int height, bool,
-                                     int, int, int, int, juce::ComboBox& box)
+void CustomLookAndFeel::drawComboBox(juce::Graphics& g, int width, int height, bool isButtonDown,
+                                     int buttonX, int buttonY, int buttonW, int buttonH, juce::ComboBox& box)
 {
     juce::Rectangle<int> boxBounds(0, 0, width, height);
 
+    // Dessinez l'arrière-plan
     g.setColour(box.findColour(juce::ComboBox::backgroundColourId));
     g.fillRect(boxBounds);
+
+    // Dessinez la bordure
+    g.setColour(juce::Colours::grey); // Choisissez la couleur que vous préférez pour la bordure
+    g.drawRect(boxBounds, 1); // Le deuxième paramètre est l'épaisseur de la bordure
 
     juce::Rectangle<int> textArea = boxBounds.reduced(4, 0);
     g.setColour(box.findColour(juce::ComboBox::textColourId));
@@ -29,3 +34,4 @@ void CustomLookAndFeel::drawComboBox(juce::Graphics& g, int width, int height, b
         g.drawText(box.getText(), textArea, juce::Justification::centred, false);
     }
 }
+
